4b2caa4b31ebb982bcd552f6005c143a803ef230
JeremyLeung <Jeremy@JeremyLungdeAir.lan>
2015-11-05 00:30:37 +0800

    first commit

---
+++ helloworld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  helloworld
+//
+//  Created by JeremyLeung on 15/11/5.
+//  Copyright (c) 2015年 JeremyLeung. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
