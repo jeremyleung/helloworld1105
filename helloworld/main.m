f0d39fa75c4a53da7be8379684bb11a543673f13
jeremyleung <460625289@qq.com>
2015-11-05 01:11:54 +0800

    Update main.m

--- helloworld/main.m
+++ helloworld/main.m
@@ -11,7 +11,7 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!  byebye........");
+        NSLog(@"Hello, World!  byebye........呵呵");
     }
     return 0;
 }
